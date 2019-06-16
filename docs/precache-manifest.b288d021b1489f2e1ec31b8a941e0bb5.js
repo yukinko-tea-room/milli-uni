64d2191baa3e16a5ca1b8dd50a1c1d3695ad5da1
はくすけ <yusk9816@gmail.com>
2019-06-16 16:22:40 +0900

    [fix]about:blankになるのを直したはず

---
+++ docs/precache-manifest.b288d021b1489f2e1ec31b8a941e0bb5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0c97afa9f4cdfa8f0bcc17466925ca8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "880b26e9245026bf295e",
+    "url": "./static/css/main.73f720ca.chunk.css"
+  },
+  {
+    "revision": "88d60641bc95e9700b62",
+    "url": "./static/js/2.6e9ac2f6.chunk.js"
+  },
+  {
+    "revision": "880b26e9245026bf295e",
+    "url": "./static/js/main.ad843e4c.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
