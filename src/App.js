f06f53fe24d8b979486cbfc7b0ace13d06553d5c
ToshihitoKon <toshihito.kon@gmail.com>
2019-06-22 20:00:07 +0900

    add selecting filter

--- src/App.js
+++ src/App.js
@@ -78,7 +78,10 @@ class IdolsSelect extends React.Component {
   render() {
     const buttons = this.props.idols.map((idol, index) => {
       if(this.state.filterType.indexOf(idolData[idol].type) === -1){
-        return null
+        if(this.state.filterType.indexOf("selecting") === -1 ||
+           this.props.selectedIdols.indexOf(idol) === -1){
+          return null
+        }
       }
       return(
         <IdolButton
@@ -96,6 +99,7 @@ class IdolsSelect extends React.Component {
           {this.filterButtonBuilder("princess")}
           {this.filterButtonBuilder("fairy")}
           {this.filterButtonBuilder("angel")}
+          {this.filterButtonBuilder("selecting")}
         </form>
         {buttons}
       </div>
@@ -180,6 +184,7 @@ class App extends React.Component {
       this.setState({ selectedIdols: selected, selectedUnits: units });
     }
   }
+
   setClassNameSuffix(suffix){
     this.setState({classNameSuffix: suffix})
   }
