557f8da8632319f008e032c91c9eb35a91f46569
ToshihitoKon <toshihito.kon@gmail.com>
2019-06-23 18:32:32 +0900

    npm run build-dataでみりゆに点数表からデータを持ってきてsrcに置くまでをやってくれるようになった

--- script/build-data.sh
+++ script/build-data.sh
@@ -8,3 +8,4 @@ wget -O unit-to-idol.csv "${SPREADSHEET_URL}?format=csv&gid=${UNIT_TO_IDOL_SHEET
 wget -O idol-to-unit.csv "${SPREADSHEET_URL}?format=csv&gid=${IDOL_TO_UNIT_SHEET_ID}"
 
 ./csv-to-json.py
+cp *.json ../src
