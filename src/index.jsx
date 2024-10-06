7bfc2489e1f717ae816dc8226cd9c061160cd61e
azurata <git@azurata.me>
2024-10-06 22:11:38 +0900

    chore: Reactのバージョンを18にした

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import * as serviceWorker from './serviceWorker.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
